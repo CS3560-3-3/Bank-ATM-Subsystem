f6ad17bd8d359d98e481d847731ad25568e5a1c2
patrickhwang21 <61051549+patrickhwang21@users.noreply.github.com>
2025-10-22 23:00:00 -0700

    Basic Bank Account Class

---
+++ BankAccount.h
@@ -0,0 +1,20 @@
+#ifndef BANKACCOUNT_H
+#define BANKACCOUNT_H
+using namespace std;
+
+#include <string>
+
+class BankAccount {
+protected:
+    string accountPin;
+    string routingNumber;
+
+public:
+    BankAccount();
+    BankAccount(const string& pin, const string& routing);
+
+    virtual ~BankAccount() = default;
+    virtual void showInfo() const = 0; 
+};
+
+#endif

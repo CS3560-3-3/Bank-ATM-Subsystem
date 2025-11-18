c7d8680bab65f70e841d229ccfce1e72327c916b
Ryan Frazin <RyanFrazin@Gmail.com>
2025-11-17 22:11:02 -0800

    Write BankAccount.cpp

--- sources/BankAccount.cpp
+++ sources/BankAccount.cpp
@@ -1,2 +1,7 @@
 #include "../headers/BankAccount.h"
+#include <iostream>
+using namespace std;
 
+BankAccount::BankAccount(const string& pin, const string& routing) : accountPin(pin), routingNumber(routing) {
+	// validation
+}

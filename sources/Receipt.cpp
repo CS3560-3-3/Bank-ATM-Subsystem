6fc94840a01a457c0d7b0740355fb5206ddbb760
Ryan Frazin <RyanFrazin@Gmail.com>
2025-11-19 18:53:18 -0800

    Create receipt source

---
+++ sources/Receipt.cpp
@@ -0,0 +1,12 @@
+#include "../headers/Receipt.h"
+#include <iostream>
+
+/*--------Parameterized Constructor--------*/
+Receipt::Receipt(double transferAmount) :
+	transferAmount(transferAmount) 
+{}
+
+/*--------Print the receipt details--------*/
+void Receipt : printReceipt() const {
+	//print receipt logic
+}
